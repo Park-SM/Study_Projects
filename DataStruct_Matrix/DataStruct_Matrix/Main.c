71e675eb8d3d9d02cf58901a350968b74025f128
Park-SM <park97.sm@gmail.com>
2019-04-16 22:49:09 +0900

    DataStruct_Matrix) Updated for TransporseMatrix

--- DataStruct_Matrix/DataStruct_Matrix/Main.c
+++ DataStruct_Matrix/DataStruct_Matrix/Main.c
@@ -28,6 +28,23 @@ int** CreateSparseMatrix(int *mMatrix, int mRow, int mCol) {
 	return (int**)sMatrix;
 }
 
+int** CreateTransposedMatrix(int *mMatrix, int mRow, int mCol) {
+	int **tMatrix = (int**)calloc(mRow * mCol, sizeof(int));
+	/*
+	for (int t = 0; t < mCol; t++) {
+		for (int i = t; i < mRow; i++) {
+			*(tMatrix + i) = *(mMatrix + (i * mCol));
+		}
+	}
+	*/
+
+	for (int y = 0; y < mRow; y++) {
+		for (int x = 0; x < mCol; x++) {
+			tMatrix[x][y] = ;	/////////////////////////////////////////// 여기까지 작업함.#####
+
+	return (int**)tMatrix;
+}
+
 // 더블포인터로 매개변수를 받을 경우 Matrix[r][c] 이런 식의 참조가 불가능하다.
 // 그 이유는 Matrix[r][c]에서 r의 값이 1씩 증가할 때마다 한 줄의 col값 만큼 건너 뛰어야하는데
 // 선언이 int **Matrix로만 되어있으므로 얼만큼 건너 뛰어야하는지 모르기 때문에 직접 설정해주어야함. 혹은 배열을 그냥 출력하면 됨.
@@ -42,11 +59,13 @@ int main() {
 
 	int MainMatrix[6][7] = { {0, 0, 1, 0, 2, 0, 0}, {0, 0, 0, 4, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 7}, {3, 0, 0, 0, 0, 0, 0}, {0, 5, 0, 0, 0, 6, 0}, {0, 0, 0, 0, 0, 0, 9} };
 	int **sMatrix = CreateSparseMatrix(MainMatrix, 6, 7);
+	int **tMatrix = CreateTransposedMatrix(sMatrix, 3, 9);
 	
 	PrintMatrix(MainMatrix, 6, 7);
 	puts("");
 	PrintMatrix(sMatrix, 9, 3);
-
+	puts("");
+	PrintMatrix(tMatrix, 3, 9);
 
 	system("PAUSE");
 	return 0;
