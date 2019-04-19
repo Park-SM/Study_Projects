07dabfb1d090aeaf29206b3c3d5e152e4e787cf7
Park-SM <park97.sm@gmail.com>
2019-04-19 12:20:59 +0900

    DataStruct - Queue Algorithm

--- DataStruct_Queue/DataStruct_Queue/Main.c
+++ DataStruct_Queue/DataStruct_Queue/Main.c
@@ -9,14 +9,13 @@ typedef struct tagNode {
 typedef struct tagQueue {
 	Node *FrontNode;
 	Node *RearNode;
+	int cpapcity;
 } Queue;
 
-Node* CreateNode() {
+Node* CreateNode(int nData) {
 	Node *NewNode = (Node*)malloc(sizeof(Node));
-	
-	NewNode->Data = NULL;
+	NewNode->Data = nData;
 	NewNode->NextNode = NULL;
-
 	return NewNode;
 }
 
@@ -24,31 +23,63 @@ void AppendNode(Queue *queue, Node *NewNode) {
 	if (queue->FrontNode == NULL) {
 		queue->FrontNode = NewNode;
 		queue->RearNode = NewNode;
-	} else {
+	}
+	else {
 		queue->RearNode->NextNode = NewNode;
 		queue->RearNode = NewNode;
 	}
+	queue->cpapcity++;
 }
 
-void PrintList(Node *List) {
-	if (List != NULL) {
-		while (List != NULL) {
-			printf("> %d\n", List->Data);
-			List = List->NextNode;
+void PrintQueue(Queue *queue) {
+	if (queue->FrontNode != NULL) {
+		Node *Current = queue->FrontNode;
+		while (Current != NULL) {
+			printf("> %d\n", Current->Data);
+			Current = Current->NextNode;
 		}
-	} else puts("");
+	}
+	else puts("> No data..");
 }
 
-void CreateQueue(Queue *queue, int capacity) {
-	if (capacity) {
-		for (int i = 0; i < capacity; i++) AppendNode(queue, CreateNode());
-		queue->RearNode->NextNode = queue->FrontNode;
-		queue->RearNode = queue->FrontNode;
-	} else puts("Error: Failed to create queue..");
+int Pop(Queue *queue) {
+	int PopedData = 0;
+	if (queue->FrontNode != NULL) {
+		PopedData = queue->FrontNode->Data;
+		Node *DeleteNode = queue->FrontNode;
+		if (queue->FrontNode == queue->RearNode) {
+			queue->FrontNode = NULL;
+			queue->RearNode = NULL;
+		}
+		else queue->FrontNode = queue->FrontNode->NextNode;
+
+		queue->cpapcity--;
+		printf("> Poped data: %d\n", PopedData);
+		free(DeleteNode);
+	}
+	else puts("> Empty queue..");
+
+	return PopedData;
 }
 
 int main() {
 
-	system("");
+	int i;
+	Queue queue = { NULL, NULL, 0 };
+
+	puts(":::: Push 연산 ::::");
+	for (i = 1; i <= 3; i++) AppendNode(&queue, CreateNode(i));
+	PrintQueue(&queue);
+
+	puts("\n:::: Pop 연산 ::::");
+	Pop(&queue);
+	Pop(&queue);
+	Pop(&queue);
+	Pop(&queue);
+
+	puts("\n:::: Queue 출력 ::::");
+	PrintQueue(&queue);
+
+	system("PAUSE");
 	return 0;
 }
