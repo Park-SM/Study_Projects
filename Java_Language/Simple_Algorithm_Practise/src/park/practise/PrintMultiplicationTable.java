7f28a16286e4ec161a9da0e3a47ec1add22747d9
Park-SM <park97.sm@gmail.com>
2019-04-21 04:21:53 +0900

    Updated about System.out.println() to System.out.printf()

--- Java_Language/Simple_Algorithm_Practise/src/park/practise/PrintMultiplicationTable.java
+++ Java_Language/Simple_Algorithm_Practise/src/park/practise/PrintMultiplicationTable.java
@@ -21,7 +21,7 @@ class MultiplicationTable {
 		for (int Dan = 2; Dan <= this.MaxDan; Dan+=Column) {
 			for (int Row = 1; Row <= this.MaxDan; Row++) {
 				for (int tDan = Dan; tDan < Dan + Column && tDan <= this.MaxDan; tDan++) {
-					System.out.print(tDan + " X " + Row + " = " + (tDan * Row) + "\t");
+					System.out.printf("%2d X %2d = %d\t", tDan, Row, tDan * Row);
 				}
 				System.out.println();
 			}
