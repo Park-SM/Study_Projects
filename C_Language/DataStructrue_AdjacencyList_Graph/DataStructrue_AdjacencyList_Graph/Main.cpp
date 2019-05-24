6d9eaf43665f62df151365d442ef7c1230d17570
Park-SM <park97.sm@gmail.com>
2019-05-24 15:49:10 +0900

    modified DataStructure_AdjacencyList_Graph

--- C_Language/DataStructrue_AdjacencyList_Graph/DataStructrue_AdjacencyList_Graph/Main.cpp
+++ C_Language/DataStructrue_AdjacencyList_Graph/DataStructrue_AdjacencyList_Graph/Main.cpp
@@ -1,11 +1,15 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
+
+typedef struct tagGraph {
+	struct tagVertex *VertexList;
+	int Capacity;
+} Graph;
 
 typedef struct tagVertex {
 	int Data;
 	int Visited;
-	int index;
+	int Index;
 
 	struct tagEdge *AdjacencyList;
 	struct tagVertex *NextVertex;
@@ -13,40 +17,41 @@ typedef struct tagVertex {
 
 typedef struct tagEdge {
 	int Weight;
-	struct tagEdge *NextEdge;
 	struct tagVertex *FromVertex;
 	struct tagVertex *TargetVertex;
+	struct tagEdge *NextEdge;
 } Edge;
 
-typedef struct tagGraph {
-	struct tagVertex *VertexList;
-	int Capacity;
-} Graph;
+Graph* CreateGraph() {
+	Graph* NewGraph = (Graph*)malloc(sizeof(Graph));
+
+	NewGraph->VertexList = NULL;
+	NewGraph->Capacity = 0;
+
+	return NewGraph;
+}
 
 Vertex* CreateVertex(int nData) {
-	Vertex *NewVertex = (Vertex*)malloc(sizeof(Vertex));
+	Vertex* NewVertex = (Vertex*)malloc(sizeof(Vertex));
+
 	NewVertex->Data = nData;
 	NewVertex->Visited = 0;
-	NewVertex->index = -1;
+	NewVertex->Index = 0;
 	NewVertex->AdjacencyList = NULL;
 	NewVertex->NextVertex = NULL;
+
 	return NewVertex;
 }
 
 Edge* CreateEdge(Vertex *FromV, Vertex *TargetV, int nWeight) {
-	Edge *NewEdge = (Edge*)malloc(sizeof(Edge));
+	Edge* NewEdge = (Edge*)malloc(sizeof(Edge));
+
 	NewEdge->FromVertex = FromV;
 	NewEdge->TargetVertex = TargetV;
 	NewEdge->Weight = nWeight;
 	NewEdge->NextEdge = NULL;
-	return NewEdge;
-}
 
-Graph* CreateGraph() {
-	Graph *NewGraph = (Graph*)malloc(sizeof(Graph));
-	NewGraph->VertexList = NULL;
-	NewGraph->Capacity = 0;
-	return NewGraph;
+	return NewEdge;
 }
 
 void AddVertex(Graph *graph, Vertex *NewVertex) {
@@ -58,32 +63,40 @@ void AddVertex(Graph *graph, Vertex *NewVertex) {
 		while (Current->NextVertex != NULL) Current = Current->NextVertex;
 		Current->NextVertex = NewVertex;
 	}
-	NewVertex->index = graph->Capacity++;
+	NewVertex->Index = graph->Capacity++;
 }
 
 void AddEdge(Graph *graph, Edge *NewEdge) {
-	Vertex *CurrentV = graph->VertexList;
-	while (CurrentV != NULL && CurrentV != NewEdge->FromVertex) CurrentV = CurrentV->NextVertex;
-	if (CurrentV != NULL) {
-		if (CurrentV->AdjacencyList == NULL)
-			CurrentV->AdjacencyList = NewEdge;
+	if (graph->VertexList != NULL) {
+		Vertex *CurrentV = graph->VertexList;
+		while (CurrentV != NULL && CurrentV != NewEdge->FromVertex) CurrentV = CurrentV->NextVertex;
+		if (CurrentV != NULL) {
+			if (CurrentV->AdjacencyList == NULL) {
+				CurrentV->AdjacencyList = NewEdge;
+			}
+			else {
+				Edge *CurrentE = CurrentV->AdjacencyList;
+				while (CurrentE->NextEdge != NULL) CurrentE = CurrentE->NextEdge;
+				CurrentE->NextEdge = NewEdge;
+			}
+		}
 		else {
-			Edge *CurrentE = CurrentV->AdjacencyList;
-			while (CurrentE->NextEdge != NULL) CurrentE = CurrentE->NextEdge;
-			CurrentE->NextEdge = NewEdge;
+			puts(">> Error:: There is not the vertex.");
+			free(NewEdge);
 		}
 	}
 }
 
 void PrintGraph(Graph *graph) {
 	if (graph->VertexList != NULL) {
+		puts(">> Notice:: [] is data value, () is weight value for the edge.\n");
 		Vertex *CurrentV = graph->VertexList;
-
+		Edge *CurrentE;
 		while (CurrentV != NULL) {
-			printf("Vertex[%d]: ", CurrentV->index);
-			Edge *CurrentE = CurrentV->AdjacencyList;
+			printf("> %d.Vertex's data is [%d] => ", CurrentV->Index, CurrentV->Data);
+			CurrentE = CurrentV->AdjacencyList;
 			while (CurrentE != NULL) {
-				printf("%d[%d] ", CurrentE->TargetVertex->Data, CurrentE->Weight);
+				printf("%d.Vertex(%d) ", CurrentE->TargetVertex->Index, CurrentE->Weight);
 				CurrentE = CurrentE->NextEdge;
 			}
 			CurrentV = CurrentV->NextVertex;
@@ -94,31 +107,27 @@ void PrintGraph(Graph *graph) {
 
 int main() {
 
-	int uData = 0;
-	printf("Graph Capacity: ");
-	scanf("%d", &uData);
-
 	Graph *graph = CreateGraph();
-	Vertex **VertexArray = (Vertex**)calloc(uData, sizeof(Vertex));
+	int NumOfVertex = 9;
+	Vertex *GraphArray[9];
 
-	puts("");
-	for (int i = 0; i < uData; i++) {
-		VertexArray[i] = CreateVertex(i + 10);
-		AddVertex(graph, VertexArray[i]);
+	for (int i = 0; i < NumOfVertex; i++) {
+		GraphArray[i] = CreateVertex(i);
+		AddVertex(graph, GraphArray[i]);
 	}
 
-	AddEdge(graph, CreateEdge(VertexArray[0], VertexArray[1], 0));
-	AddEdge(graph, CreateEdge(VertexArray[0], VertexArray[2], 0));
-	AddEdge(graph, CreateEdge(VertexArray[1], VertexArray[2], 0));
-	AddEdge(graph, CreateEdge(VertexArray[1], VertexArray[3], 0));
-	AddEdge(graph, CreateEdge(VertexArray[2], VertexArray[3], 0));
-	AddEdge(graph, CreateEdge(VertexArray[3], VertexArray[4], 0));
-	AddEdge(graph, CreateEdge(VertexArray[4], VertexArray[5], 0));
-	AddEdge(graph, CreateEdge(VertexArray[4], VertexArray[6], 0));
-	AddEdge(graph, CreateEdge(VertexArray[5], VertexArray[6], 0));
+	AddEdge(graph, CreateEdge(GraphArray[0], GraphArray[1], 0));
+	AddEdge(graph, CreateEdge(GraphArray[0], GraphArray[2], 0));
+	AddEdge(graph, CreateEdge(GraphArray[1], GraphArray[2], 0));
+	AddEdge(graph, CreateEdge(GraphArray[1], GraphArray[3], 0));
+	AddEdge(graph, CreateEdge(GraphArray[2], GraphArray[4], 0));
+	AddEdge(graph, CreateEdge(GraphArray[3], GraphArray[6], 0));
+	AddEdge(graph, CreateEdge(GraphArray[4], GraphArray[5], 0));
+	AddEdge(graph, CreateEdge(GraphArray[4], GraphArray[6], 0));
+	AddEdge(graph, CreateEdge(GraphArray[4], GraphArray[7], 0));
+	AddEdge(graph, CreateEdge(GraphArray[6], GraphArray[8], 0));
 
 	PrintGraph(graph);
-	printf("\nGraph Capacity: %d..\n", graph->Capacity);
 
 	system("PAUSE");
 	return 0;
