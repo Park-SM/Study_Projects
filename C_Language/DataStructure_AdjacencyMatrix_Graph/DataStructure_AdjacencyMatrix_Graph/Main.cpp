eff43f026425be3827aa5c2b1197fe56d593d973
Park-SM <park97.sm@gmail.com>
2019-05-27 00:15:46 +0900

    Update for DataStructure_AdjacencyMatrix_Graph

--- C_Language/DataStructure_AdjacencyMatrix_Graph/DataStructure_AdjacencyMatrix_Graph/Main.cpp
+++ C_Language/DataStructure_AdjacencyMatrix_Graph/DataStructure_AdjacencyMatrix_Graph/Main.cpp
@@ -17,6 +17,11 @@ Graph* CreateGraph(int uNumOfVertex) {
 	return NewGraph;
 }
 
+void InputVertexData(Graph *graph, int VertexIndex, int nData) {
+	if (VertexIndex >= 0 && VertexIndex < graph->NumOfVertex) graph->VertexArray[VertexIndex] = nData;
+	else puts(">> Error:: Invalid Index.");
+}
+
 void AddEdge_directed(Graph *graph, int FromV, int TargetV) {
 	if (graph != NULL) *(graph->AdjacencyMatrix + (FromV * graph->NumOfVertex) + TargetV) = 1;
 	else puts(">> Error:: Failed to add Edge.");
@@ -32,11 +37,11 @@ void AddEdge_undirected(Graph *graph, int FromV, int TargetV) {
 void PrintGraph(Graph *graph) {
 	if (graph != NULL) {
 		printf("    ");
-		for (int Row = 0; Row < graph->NumOfVertex; Row++) printf(" %2d ", Row); printf("\n    ");
+		for (int Row = 0; Row < graph->NumOfVertex; Row++) printf(" %2d ", graph->VertexArray[Row]); printf("\n    ");
 		for (int Row = 0; Row < graph->NumOfVertex; Row++) printf(" ==="); puts("");
 
 		for (int Col = 0; Col < graph->NumOfVertex; Col++) {
-			printf("%2d||", Col);
+			printf("%2d||", graph->VertexArray[Col]);
 			for (int Row = 0; Row < graph->NumOfVertex; Row++) {
 				printf(" %2d ", *(graph->AdjacencyMatrix + (Col * graph->NumOfVertex) + Row));
 			}
@@ -47,6 +52,8 @@ void PrintGraph(Graph *graph) {
 
 int main() {
 	Graph *graph = CreateGraph(9);
+
+	for (int i = 0; i < 9; i++) InputVertexData(graph, i, i);
 
 	AddEdge_undirected(graph, 0, 1);
 	AddEdge_undirected(graph, 0, 2);
